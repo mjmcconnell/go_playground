d213d595c381195774dc43460858a435bfda6a9b
Mark McConnell <mmcconnell@equiis.com>
2019-09-02 11:45:59 +0100

    templated response added

--- app/main.go
+++ app/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"github.com/gorilla/mux"
 	"net/http"
 )
+
+type Todo struct {
+	Title string
+	Done bool
+}
+
+type TodoPageData struct {
+    PageTitle string
+    Todos     []Todo
+}
+
+
 
 func main() {
 	router := mux.NewRouter()
@@ -17,10 +30,24 @@ func main() {
 	testRouter.HandleFunc("", func(response http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(response, "Test, %s", request.URL.Path)
 	})
-	testRouter.HandleFunc("/{foo}", func(response http.ResponseWriter, request *http.Request) {
-		urlVars := mux.Vars(request)
 
-		fmt.Fprintf(response, "Test, %s", urlVars["foo"])
+	testRouter.HandleFunc("/todo", func(response http.ResponseWriter, request *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/todo.html"))
+
+		data := TodoPageData{
+		    PageTitle: "My TODO list",
+		    Todos: []Todo{
+		        {Title: "Task 1", Done: false},
+		        {Title: "Task 2", Done: true},
+		        {Title: "Task 3", Done: true},
+		    },
+		}
+		tmpl.Execute(response, data)
+
+	})
+	router.HandleFunc("/{path}", func(response http.ResponseWriter, request *http.Request) {
+		urlVars := mux.Vars(request)
+		fmt.Fprintf(response, "404, could not find path: %s", urlVars["path"])
 	})
 
 	http.ListenAndServe(":8080", router)
